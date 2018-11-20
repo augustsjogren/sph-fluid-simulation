c97a053f50153cee22caeb2987196a60d6c0814b
August Sjögren <august.sjogren@gmail.com>
2018-11-20 10:28:17 +0100

    Added basic gravity

--- particles.py
+++ particles.py
@@ -8,20 +8,51 @@ print 'Particles selected'
 
 # print str(len(particleList)) + " particles selected"
 
+# Clear keyframes
+cmds.cutKey()
+
 # Animation
 startTime = cmds.playbackOptions(query=True, minTime=True)
 endTime = cmds.playbackOptions(query=True, maxTime=True)
 
 currentTime = startTime
 
-noParticles = 100
+noParticles = 25
 
-spawnHeight = 5
+spawnHeight = 3
+
+gravity = -9.82
+
+mass = 0.00001
+
 
 # Set keyframes for animating the particles
 while currentTime < endTime:
-    newHeight = spawnHeight - i * 0.01
-    cmds.setKeyframe('myParticle*', attribute='translateY', v=newHeight, t=currentTime)
+    #newHeight = spawnHeight - currentTime * 0.01
+    #cmds.setKeyframe('myParticle*', attribute='translateY', v=newHeight, t=currentTime)
+
+    # Main simulaiton loop
+    for p in range(noParticles):
+
+        particleIndex = p + 1;
+
+        # Get current position
+        currentPosition = [ 
+            cmds.getAttr('myParticle' + str(particleIndex) + '.translateX'), 
+            cmds.getAttr('myParticle' + str(particleIndex) + '.translateY'), 
+            cmds.getAttr('myParticle' + str(particleIndex) + '.translateZ')
+        ]
+
+        # Apply gravity
+        velocity = gravity * currentTime
+
+        nextPosition = currentPosition
+
+        nextPosition[1] =  currentPosition[1] + (velocity * currentTime * mass)
+
+        cmds.setKeyframe('myParticle*', attribute='translateY', v=nextPosition[1], t=currentTime)
+
+
     currentTime += 1
 
 print 'Keyframes set'
--- setupScene.py
+++ setupScene.py
@@ -15,10 +15,10 @@ cmds.polyCube(sx=10, sy=15, sz=5, w=5, d=5, h=0.01, name='ground')
 noParticles = 0
 
 # Create particles
-for i in range(10):
-    for j in range(10):
+for i in range(5):
+    for j in range(5):
         particle = cmds.polySphere(n='myParticle#', sx=6, sy=6, r=0.05)
-        cmds.move(i * 0.1, 5, j * 0.1, particle)
+        cmds.move(i * 0.1, 3, j * 0.1, particle)
         noParticles += 1
 
 #particleList = cmds.ls('myParticle*')
