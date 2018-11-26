84b02a7c07d2fd6d55a386806bd968e76896afa9
August Sjögren <august.sjogren@gmail.com>
2018-11-26 11:23:37 +0100

    Particles are now falling down
    Particles now fall down with gravity

--- particles.py
+++ particles.py
@@ -18,37 +18,31 @@ cmds.polyCube(sx=10, sy=15, sz=5, w=5, d=5, h=0.01, name='ground')
 
 noParticles = 0
 
-# Create particles
+# Create  a cube of particles
 for i in range(5):
     for j in range(5):
-        particle = cmds.polySphere(n='myParticle#', sx=6, sy=6, r=0.05)
-        cmds.move(i * 0.1, 3, j * 0.1, particle)
-        noParticles += 1
+        for k in range(5):
+            particle = cmds.polySphere(n='myParticle#', sx=2, sy=2, r=0.05)
+            cmds.move(i * 0.1, 2 + j * 0.1, k * 0.1, particle)
+            noParticles += 1
 
 #particleList = cmds.ls('myParticle*')
 print str(noParticles) + " particles created"
 
-#-------SETUP DONE-----------------
-
-
+# -------SETUP DONE-----------------
 
 particleList = cmds.ls('myParticle*', type='transform', showType=False)
 
-# Print all particles in the scene
-# for part in particleList:
-#     print(part)
-
 # Select all particles
-cmds.select('myParticle*')
+# cmds.select('myParticle*')
 # print 'Particles selected'
-
 # print str(len(particleList)) + " particles selected"
 
 # Clear keyframes
 cmds.cutKey()
 
 class Particle:
-    mass = 0.0001
+    mass = 0.01
 
     def __init__(self):
         self.name = "unset"
@@ -60,116 +54,103 @@ class Particle:
 # --------------- Functions -----------------
 
 # Get all neighbors within the smoothing radius
-def getNeighbors( particle, smoothingLength ):
-    
-    neighbours = []
 
-    # currentPosition = [ cmds.getAttr( particle.name + '.translateX' ), cmds.getAttr( particle.name + '.translateY' ), cmds.getAttr( particle.name + '.translateZ' ) ]
+
+def getNeighbors(particle, smoothingLength):
+
+    neighbours = []
 
     index = 0
 
     for otherParticle in particles:
-        
-        # particlePosition = [ cmds.getAttr( otherParticle + '.translateX' ), 
-        # cmds.getAttr( otherParticle + '.translateY' ),
-        # cmds.getAttr( otherParticle + '.translateZ' ) ]
-        
+
         deltadist = [0, 0, 0]
-        
-        # Calculate distance 
-        # deltadist[0] = particlePosition[0] - currentPosition[0]
-        # deltadist[1] = particlePosition[1] - currentPosition[1]
-        # deltadist[2] = particlePosition[2] - currentPosition[2]
 
         deltadist[0] = particle.position[0] - otherParticle.position[0]
         deltadist[1] = particle.position[1] - otherParticle.position[1]
         deltadist[2] = particle.position[2] - otherParticle.position[2]
 
+        distance = math.sqrt(math.pow( deltadist[0], 2) + math.pow(deltadist[1], 2) + math.pow(deltadist[2], 2))
 
-        distance = math.sqrt( math.pow(deltadist[0], 2) + math.pow(deltadist[1], 2) + math.pow(deltadist[2], 2) )
-    
         # if distance <= smoothingLength, put in array
-        if distance <= smoothingLength and distance > 0: 
-            #neighbours.append( otherParticle )
-            # print(particles[index].name)
-            neighbours.append( particles[index] )
+        if distance <= smoothingLength and distance > 0:
+            neighbours.append(particles[index])
 
         index += 1
 
-    
     return neighbours
+
 
 def applyPoly6Kernel(particleDistance):
     smoothingRadius = 0.2
-    output = (315/(64 * math.pi * math.pow(smoothingRadius, 9))) * math.pow((math.pow(smoothingRadius, smoothingRadius) - math.pow(particleDistance, particleDistance)), 3)
+
+    output = ((315/(64 * math.pi * math.pow(smoothingRadius, 9))) *
+              math.pow((math.pow(smoothingRadius, 2) -
+                        math.pow(particleDistance, 2)), 3)
+              )
+
     return output
 
 # Calculate the density based on all neighbors within the smoothingRadius
-def calculateDensity(particle,  particleMass, neighbors ):
+def calculateDensity(particle,  particleMass, neighbors):
 
     # print(neighbors)
-    
-    density = 1
+
+    density = 40
     for neighbor in neighbors:
         distanceToNeighbor = calculateDistance(particle, neighbor)
         density += particleMass * applyPoly6Kernel(distanceToNeighbor)
+
+    if density < 40:
+        density = 40
+
     return density
 
 
 def calculatePressure(particleDensity):
-    p0 = 100
-    K = 20
+    p0 = 40
+    K = 40
     pressure = K * (particleDensity - p0)
+
+    # print(pressure)
     return pressure
 
 
 def calculateDistance(p1, p2):
-    # print(p2.name)
 
-    p1Pos = [ cmds.getAttr( p1.name + '.translateX' ), 
-        cmds.getAttr( p1.name + '.translateY' ),
-        cmds.getAttr( p1.name + '.translateZ' ) ]
+    distanceVector = getDistanceVector(p1, p2)
 
-    p2Pos = [ cmds.getAttr( p2.name + '.translateX' ), 
-        cmds.getAttr( p2.name + '.translateY' ),
-        cmds.getAttr( p2.name + '.translateZ' ) ]
+    distance = math.sqrt(math.pow(
+        distanceVector[0], 2) + math.pow(distanceVector[1], 2) + math.pow(distanceVector[2], 2))
 
-    # print(cmds.getAttr( p2.name + '.translateX' ))
-    
-
-    distanceVector = [
-            p1Pos[0] - p2Pos[0],
-            p1Pos[1] - p2Pos[1],
-            p1Pos[2] - p2Pos[2]]
-
-    distance = math.sqrt( math.pow(distanceVector[0], 2) + math.pow(distanceVector[1], 2) + math.pow(distanceVector[2], 2) )
+    # print(distance)
 
     return distance
 
 
 # Input as strings expected
 def getDistanceVector(p1, p2):
-    p1Pos = [ cmds.getAttr( str(p1.name) + '.translateX' ), 
-    cmds.getAttr( str(p1.name) + '.translateY' ),
-    cmds.getAttr( str(p1.name) + '.translateZ' ) ]
-
-    p2Pos = [ cmds.getAttr( str(p2.name) + '.translateX' ), 
-        cmds.getAttr( str(p2.name) + '.translateY' ),
-        cmds.getAttr( str(p2.name) + '.translateZ' ) ]
 
     distanceVector = [
-            p1Pos[0] - p2Pos[0],
-            p1Pos[1] - p2Pos[1],
-            p1Pos[2] - p2Pos[2] ]
+        p1.position[0] - p2.position[0],
+        p1.position[1] - p2.position[1],
+        p1.position[2] - p2.position[2]]
+
+    # print(distanceVector)
 
     return distanceVector
 
+
 def calculateSpikyKernel(smoothingRadius, particle, neighbor):
+
     distanceVec = getDistanceVector(particle, neighbor)
 
-    gX = -( 45 / (math.pi * math.pow(smoothingRadius, 6)) ) * math.pow((smoothingRadius - distanceVec[0]), 2)
-    gY = -( 45 / (math.pi * math.pow(smoothingRadius, 6)) ) * math.pow((smoothingRadius - distanceVec[1]), 2)
-    gZ = -( 45 / (math.pi * math.pow(smoothingRadius, 6)) ) * math.pow((smoothingRadius - distanceVec[2]), 2)
+    gX = (45 / (math.pi * math.pow(smoothingRadius, 6))) * \
+        math.pow((smoothingRadius - distanceVec[0]), 2)
+    gY = (45 / (math.pi * math.pow(smoothingRadius, 6))) * \
+        math.pow((smoothingRadius - distanceVec[1]), 2)
+    gZ = (45 / (math.pi * math.pow(smoothingRadius, 6))) * \
+        math.pow((smoothingRadius - distanceVec[2]), 2)
 
     gradient = [gX, gY, gZ]
     return gradient
@@ -178,14 +159,18 @@ def calculateSpikyKernel(smoothingRadius, particle, neighbor):
 def calculateAcceleration(neighbors, particle, particleMass, particlePressure, particleDensity):
     acceleration = [0, 0, 0]
 
-    for neighbor in neighbors:
-        #normalizedDirection = getDistanceVector(particle, neighbor)
-        # print(neighbor)
-        spikyGradient = calculateSpikyKernel(0.2, particle, neighbor)
-        acceleration[0] = -(particleMass/particleMass) * ( (particlePressure + particle.pressure) / ( 2*particleDensity*neighbor.density ) ) * spikyGradient[0]
-        acceleration[1] = -(particleMass/particleMass) * ( (particlePressure + particle.pressure) / ( 2*particleDensity*neighbor.density ) ) * spikyGradient[1]
-        acceleration[2] = -(particleMass/particleMass) * ( (particlePressure + particle.pressure) / ( 2*particleDensity*neighbor.density ) ) * spikyGradient[2]
+    # print(particlePressure)
+    # print(particle.pressure)
 
+    for neighbor in neighbors:
+        spikyGradient = calculateSpikyKernel(0.2, particle, neighbor)
+
+        acceleration[0] = -(particleMass/particleMass) * ((neighbor.pressure +
+                                                           particle.pressure) / (2*particleDensity*neighbor.density)) * spikyGradient[0]
+        acceleration[1] = -(particleMass/particleMass) * ((neighbor.pressure +
+                                                           particle.pressure) / (2*particleDensity*neighbor.density)) * spikyGradient[1]
+        acceleration[2] = -(particleMass/particleMass) * ((neighbor.pressure +
+                                                           particle.pressure) / (2*particleDensity*neighbor.density)) * spikyGradient[2]
     return acceleration
 
 
@@ -197,20 +182,17 @@ currentTime = startTime
 noParticles = len(particleList)
 spawnHeight = 3
 gravity = [0, -9.82, 0]
-# mass = 0.00001
-mass = 0.00001
 
 # Create a progress bar
 window = cmds.window()
 cmds.columnLayout()
 cmds.text(label='Simulation Progress')
-progressControl = cmds.progressBar(status='"Simulation Progress', maxValue=noParticles*endTime, width=300)
-# cmds.button( label='Simulation Progress!', command='cmds.progressBar(progressControl, edit=True, step=1)' )
+progressControl = cmds.progressBar(
+    status='"Simulation Progress', maxValue=noParticles*endTime, width=300)
 
 # Start timer
 start = cmds.timerX()
 cmds.showWindow(window)
-
 
 index = 0
 particles = []
@@ -220,20 +202,18 @@ for i in particleList:
     p = Particle()
     p.particleIndex = index
     p.name = particleList[index]
-    # cmds.setAttr( particleList[index] + '.translateX', 3 )
-    cmds.setAttr( particleList[index] + '.translateY', 3 )
-    # cmds.setAttr( particleList[index] + '.translateZ', 3 )
     particles.append(p)
     index += 1
 
+
 def initialSetup():
-    # Initial position
+    # Set initial positions
     loopindex = 1
     for p in particles:
-        p.position = [ 
-            cmds.getAttr('myParticle' + str(loopindex) + '.translateX'), 
-            cmds.getAttr('myParticle' + str(loopindex) + '.translateY'), 
-            cmds.getAttr('myParticle' + str(loopindex) + '.translateZ') ]       
+        p.position = [
+            cmds.getAttr('myParticle' + str(loopindex) + '.translateX'),
+            cmds.getAttr('myParticle' + str(loopindex) + '.translateY'),
+            cmds.getAttr('myParticle' + str(loopindex) + '.translateZ')]
         # print(p.position)
         loopindex += 1
 
@@ -252,44 +232,56 @@ while currentTime < endTime:
         currentParticle = particles[p]
         # currentParticle.name = particleList[p]
 
+        # print(currentParticle.position)
+
         # --------- Calculate! -------------
 
         particleNeighbors = getNeighbors(currentParticle, 0.2)
 
-        currentParticle.density = calculateDensity(currentParticle, mass, particleNeighbors)
+        # print(particleNeighbors)
 
-        currentParticle.pressure = calculatePressure(currentParticle.density)
+        currentParticle.density = calculateDensity(
+            currentParticle, currentParticle.mass, particleNeighbors)
 
         # print(currentParticle.density)
 
-        currentParticle.acceleration = calculateAcceleration(particleNeighbors, currentParticle, mass, currentParticle.pressure, currentParticle.density)
+        currentParticle.pressure = calculatePressure(currentParticle.density)
+
+        # print(currentParticle.pressure)
+
+        currentParticle.acceleration = calculateAcceleration(
+            particleNeighbors, currentParticle, currentParticle.mass, currentParticle.pressure, currentParticle.density)
 
         # print(currentParticle.acceleration)
 
         totalAcceleration = currentParticle.acceleration
         totalAcceleration[1] += gravity[1]
 
-        print(totalAcceleration)
+        # print(totalAcceleration)
 
         # Apply gravity
-        velocity = [i * (currentTime / 100) for  i in totalAcceleration] 
+        velocity = [i * (currentTime / 100) for i in totalAcceleration]
 
         # print(velocity)
-        
-        # velocity = totalAcceleration * currentTime
 
         nextPosition = currentParticle.position
         # print(nextPosition)
-        nextPosition[0] = currentParticle.position[0] + (velocity[0] * currentTime * mass)
-        nextPosition[1] = currentParticle.position[1] + (velocity[1] * currentTime * mass)
-        nextPosition[2] = currentParticle.position[2] + (velocity[2] * currentTime * mass)
+        nextPosition[0] = currentParticle.position[0] + \
+            (velocity[0] * currentTime * mass)
+        nextPosition[1] = currentParticle.position[1] + \
+            (velocity[1] * currentTime * mass)
+        nextPosition[2] = currentParticle.position[2] + \
+            (velocity[2] * currentTime * mass)
 
         currentParticle.position = nextPosition
 
         # Set keyframes for animating the particles
-        cmds.setKeyframe('myParticle*', attribute='translateX', v=nextPosition[0], t=currentTime)
-        cmds.setKeyframe('myParticle*', attribute='translateY', v=nextPosition[1], t=currentTime)
-        cmds.setKeyframe('myParticle*', attribute='translateZ', v=nextPosition[2], t=currentTime)
+        cmds.setKeyframe(currentParticle.name, attribute='translateX',
+                         v=nextPosition[0], t=currentTime)
+        cmds.setKeyframe(currentParticle.name, attribute='translateY',
+                         v=nextPosition[1], t=currentTime)
+        cmds.setKeyframe(currentParticle.name, attribute='translateZ',
+                         v=nextPosition[2], t=currentTime)
 
         # Update progress bar
         cmds.progressBar(progressControl, edit=True, step=1)
